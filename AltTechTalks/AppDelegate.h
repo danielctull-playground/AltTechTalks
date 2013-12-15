a7276d421ab1c950c2f155b31d09d895232b66ad
Daniel Tull <dt@danieltull.co.uk>
2013-12-15 17:27:49 +0000

    Initial Commit

---
+++ AltTechTalks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AltTechTalks
+//
+//  Created by Daniel Tull on 15.12.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
