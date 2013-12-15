2107f1655c48620246eb5eb40bd3be0b84826e06
Daniel Tull <dt@danieltull.co.uk>
2013-12-15 17:53:18 +0000

    Add Parser target

---
+++ AltTechTalks Parser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AltTechTalks Parser
+//
+//  Created by Daniel Tull on 15.12.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
